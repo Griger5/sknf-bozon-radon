4744b303b6d60af777356450640a5564c8477a3b
Griger5 <griger04@gmail.com>
2025-10-20 20:32:37 +0200

    add filtering for building types

--- create_map.py
+++ create_map.py
@@ -1,13 +1,24 @@
 import folium
+from folium.plugins import TagFilterButton
 import pandas as pd
 import numpy as np
 import branca.colormap as cmp
 
 m = folium.Map([50, 20], tiles="OpenStreetMap")
 
-coords_df = pd.read_excel("DETEKTORY DANE.xlsx")
+main_excel = pd.ExcelFile("DETEKTORY DANE.xlsx")
+
+coords_df = pd.read_excel(main_excel, "Sheet1")
 coords_df = coords_df.dropna(subset=["Latitude", "Longitude"])
 detector_df = pd.read_excel("wyniki.xlsx").set_index("Detector ID")
+categories_df = pd.read_excel(main_excel, "Info_Bool").fillna(0)
+
+age_categories = ["<1900", "1900-1920", "1920-1940", "1940-1960", "1960-1980", "1980-2000", "2000-2020", "2020-2040"]
+building_categories = ["blok", "kamienica", "szeregowy", "wolnostojący"]
+material_categories = ["cegła", "beton", "pustak", "drewno", "kamień", "styropian", "wylewka", "wielka płyta"]
+connections_categories = ["woda", "gaz", "kanalizacja", "wentylacja", "CO", "klimatyzacja"]
+
+categories = [*age_categories, *building_categories, *material_categories, *connections_categories]
 
 colors = ["green", "lightgreen", "orange", "red", "black"]
 
@@ -21,16 +32,20 @@ cmp.StepColormap(
     caption="Wartość gęstości śladu [ślad / mm^2]"
 ).add_to(m)
 
+TagFilterButton(categories).add_to(m)
+
 for i in range(len(coords_df)):
     try: 
         density = detector_df.loc[coords_df.iloc[i]["Nr detektora"].replace(" ", ""), "Track density"]
     except:
         denisty = "?"
-        
+
     if density != "?":
         color = colors[int(np.digitize(density, density_bins)) - 1]
     else:
         color = "blue"
+
+    building = str(coords_df.iloc[i]["Typ budynku"]).removesuffix("/blok")
 
     html=f"""
         <h1>{coords_df.iloc[i]["Nr detektora"]}</h1>
@@ -49,7 +64,8 @@ for i in range(len(coords_df)):
     folium.Marker(
         location=[coords_df.iloc[i]["Latitude"], coords_df.iloc[i]["Longitude"]],
         popup=popup,
-        icon=folium.Icon(color=color)
+        icon=folium.Icon(color=color),
+        tags=[building]
     ).add_to(m)
 
 m.save("map.html")
